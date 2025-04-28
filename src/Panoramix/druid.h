4d794033b648006ca2aa8a735670aeb6c3b6264d
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-04-28 14:17:31 +0200

    feat(druid): implement druid with refill logic

---
+++ src/Panoramix/druid.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** Panoramix [WSL: Ubuntu]
+** File description:
+** druid
+*/
+
+#ifndef DRUID_H_
+    #define DRUID_H_
+
+    #include <pthread.h>
+
+int create_druid(pthread_t *druid_thread);
+#endif /* !DRUID_H_ */

786b2ef7b9d4ea3e454820b86c3193cb1ba4149b
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-04-28 14:17:51 +0200

    refactor(main): simplify argument parsing and remove usage print function

--- src/main.c
+++ src/main.c
@@ -5,27 +5,14 @@
 ** main
 */
 
-#include <stdio.h>
 #include "Parser/parser.h"
-
-static void print_usage(void)
-{
-    printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> ");
-    printf("<nb_refills>\n");
-}
+#include "Panoramix/panoramix.h"
 
 int main([[maybe_unused]] int ac, [[maybe_unused]] char **av)
 {
-    args_t *args = NULL;
+    args_t args;
 
-    if (ac != 5) {
-        print_usage();
+    if (parse_args(ac, av, &args) == 84)
         return 84;
-    }
-    args = parse_args(av);
-    if (args == NULL) {
-        print_usage();
-        return 84;
-    }
-    return 0;
+    return run_panoramix(&args);
 }

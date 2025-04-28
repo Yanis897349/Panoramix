73ba28ed44d0180a23fc6193647e3866bbea4a22
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-04-28 10:34:10 +0200

    feat(src): Initial commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** Panoramix [WSL: Ubuntu]
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main([[maybe_unused]] int ac, [[maybe_unused]] char **av)
+{
+    printf("Hello, World!\n");
+    return 0;
+}

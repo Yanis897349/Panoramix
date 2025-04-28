8c3f6bfd5d594eeeb6a712fe690b4d34e54e125b
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-04-28 10:48:13 +0200

    feat(main): implement argument parsing and usage instructions

--- src/main.c
+++ src/main.c
@@ -6,9 +6,26 @@
 */
 
 #include <stdio.h>
+#include "Parser/parser.h"
+
+static void print_usage(void)
+{
+    printf("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> ");
+    printf("<nb_refills>\n");
+}
 
 int main([[maybe_unused]] int ac, [[maybe_unused]] char **av)
 {
-    printf("Hello, World!\n");
+    args_t *args = NULL;
+
+    if (ac != 5) {
+        print_usage();
+        return 84;
+    }
+    args = parse_args(av);
+    if (args == NULL) {
+        print_usage();
+        return 84;
+    }
     return 0;
 }
